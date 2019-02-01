6749767b3444c1ce54aeac96b3e79d3a8cd6ff7b
mariaoca <mariaolidcamarena@gmail.com>
2019-02-01 11:54:18 +0100

    chore: upload CardDetail structure

---
+++ src/components/Detail/CardDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class CardDetail extends Component {
+  render() {
+    return (
+      <div>
+      {/* Aquí se importarán las demás   */}
+      </div>
+    )
+  }
+}
+
+export default App;
