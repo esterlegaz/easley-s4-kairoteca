b678edfd73402f60ebe55a143da5c59e282007f3
inesppino <inesp.pino@gmail.com>
2019-02-01 11:37:02 +0100

    Remove unnecesary folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
-
 import api from "./api"
 
 class App extends Component {
@@ -10,18 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     )
