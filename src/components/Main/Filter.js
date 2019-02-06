ca686bc26dada08414ef8517cc0087195a9de6cb
Katherine Briceño <kath.gbl@gmail.com>
2019-02-06 13:45:47 +0100

    Chore: needs to commit to switch branches

--- src/components/Main/Filter.js
+++ src/components/Main/Filter.js
@@ -3,9 +3,16 @@ import React, {Component} from "react";
 class Filter extends Component {
   render() {
     return (
-        <label htmlFor="filter">
-            <input id="filter" type="text" placeholder="Buscar" onKeyUp={this.props.getFilter}/>
-        </label>
+        <div className="mdc-text-field mdc-text-field--outlined">
+            <input type="text" id="filter" placeholder="Buscar" onKeyUp={this.props.getFilter} className="mdc-text-field__input"/>
+                <div className="mdc-notched-outline">
+                    <div className="mdc-notched-outline__leading"></div>
+                    <div className="mdc-notched-outline__notch">
+                        <label htmlFor="filter" className="mdc-floating-label"></label>
+                    </div>
+                    <div className="mdc-notched-outline__trailing"></div>
+                </div>
+        </div>
     );
   }
 }
