d89571a959eea0572fc97c1249da81ea5ef6a102
Katherine Briceño <kath.gbl@gmail.com>
2019-02-07 10:05:19 +0100

    Feat: Create form with Google styles

--- src/components/Main/Form.js
+++ src/components/Main/Form.js
@@ -5,26 +5,61 @@ import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 
+const types = [
+    {
+        value: 'digital',
+        label: 'digital',
+    },
+    {
+        value: 'físico',
+        label: 'físico',
+    },
+]
+
+const state = [
+    {
+        value: 'Disponible',
+        label: 'Disponible',
+    },
+    {
+        value: 'Prestado',
+        label: 'Prestado',
+    },
+    {
+        value: 'Pendiente',
+        label: 'Pendiente',
+    },
+]
+
 class Form extends Component {
   render() {
     return (
         <form action="/signup" method="post">
             <TextField id="outlined-title" label="Título" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
 
-        <label className="form__label" htmlFor="title">Título</label>
-        <input className="form__input" id="title" type="text" placeholder="Escribe el título"/>
-        <label className="form__label" htmlFor="author">Autor</label>
-        <input className="form__input" id="author" type="text" placeholder="Escribe el autor"/>
-        <label className="form__label" htmlFor="ISBN">ISBN</label>
-        <input className="form__input" id="ISBN" type="text" placeholder="Escribe el ISBN"/>
-        <label className="form__label" htmlFor="type">Elige</label>
-        <select className="form__select" id="type">
-          <option>Digital></option>/option>
-          <option>Físico</option>
-        </select>
-        <label className="form__label" htmlFor="tags">Tags</label>
-        <input className="form__input" id="tags" type="text" placeholder="Escribe el título"/>
-        </form>
+            <TextField id="outlined-author" label="Autor" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
+
+            <TextField id="outlined-isbn" label="ISBN" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
+
+            <TextField id="outlined-select-types" select label="Tipo" className="form__textfield" /*value={this.state.currency} onChange={this.handleChange('currency')} */ margin="normal" variant="outlined">
+                {types.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+
+            <TextField id="outlined-tags" label="Tags" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
+
+            <TextField id="outlined-select-state" select label="Estado" className="form__textfield" /*value={this.state.currency} onChange={this.handleChange('currency')} */ margin="normal" variant="outlined">
+                {state.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+
+            </form>
     );
   }
 }
