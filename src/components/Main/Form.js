bc5577b6da612b6ff43f8f9f54850cddf5f31ba0
inesppino <inesp.pino@gmail.com>
2019-02-08 11:39:58 +0100

    Feature: add chip with suggestions

--- src/components/Main/Form.js
+++ src/components/Main/Form.js
@@ -1,9 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+// import Chip from '@material-ui/core/Chip';
+import Chips from 'react-chips';
 
 const types = [
     {
@@ -32,40 +35,53 @@ const state = [
 ]
 
 class Form extends Component {
-  render() {
-    return (
-        <div className="form__container">
-        <div className="popup">
-            <form action="/signup" method="post">
-                <TextField id="outlined-title" label="Título" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
+    state = {
+        chipData: [
+        ],
+    };
 
-                <TextField id="outlined-author" label="Autor" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
+    handleChip = chipData => {
+        this.setState({
+            chipData
+        })
+    }
 
-                <TextField id="outlined-isbn" label="ISBN" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
+    render() {
+        return (
+            <div className="form__container">
+                <div className="popup">
+                    <form action="/signup" method="post">
+                        <TextField id="outlined-title" label="Título" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
 
-                <TextField id="outlined-select-types" select label="Tipo" className="form__textfield" /*value={this.state.currency} onChange={this.handleChange('currency')} */ margin="normal" variant="outlined">
-                    {types.map(option => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
+                        <TextField id="outlined-author" label="Autor" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
 
-                <TextField id="outlined-tags" label="Tags" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
+                        <TextField id="outlined-isbn" label="ISBN" className='form__textfield' /* value={this.state.name}  onChange={this.handleChange("name")}*/ margin="normal" variant="outlined" />
 
-                <TextField id="outlined-select-state" select label="Estado" className="form__textfield" /*value={this.state.currency} onChange={this.handleChange('currency')} */ margin="normal" variant="outlined">
-                    {state.map(option => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                </form>
-            <button className="close" onClick={this.props.togglePopup}> Cerrar </button>
-        </div>    
-        </div>
-    );
-  }
+                        <TextField id="outlined-select-types" select label="Tipo" className="form__textfield" /*value={this.state.currency} onChange={this.handleChange('currency')} */ margin="normal" variant="outlined">
+                            {types.map(option => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <div>
+                            <Chips value={this.state.chipData} onChange={this.handleChip} suggestions={['JavaScript', 'React', 'Programming']}/>
+                        </div>
+
+                        <TextField id="outlined-select-state" select label="Estado" className="form__textfield" /*value={this.state.currency} onChange={this.handleChange('currency')} */ margin="normal" variant="outlined">
+                            {state.map(option => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+
+                    </form>
+                    <button className="close" onClick={this.props.togglePopup}> Cerrar </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Form;
