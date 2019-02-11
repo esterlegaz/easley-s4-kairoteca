4d960dfd975d571c35dbd5cacbac8c48f4ba556b
inesppino <inesp.pino@gmail.com>
2019-02-11 14:57:40 +0100

    fix: conflicts

--- src/components/Layout/Main.js
+++ src/components/Layout/Main.js
@@ -8,14 +8,8 @@ class Main extends Component {
     return (
         <Fragment>
             <Filter getFilter={this.props.getFilter} />
-<<<<<<< HEAD
-            <button onClick={this.props.togglePopup}> Añadir libro </button>
-            <List bookList={this.props.bookList} haveBooks={this.props.haveBooks} handleLoan={this.props.handleLoan}/>
-        </Fragment>   
-=======
             <List popId={this.props.popId} toggleDeletePopup={this.props.toggleDeletePopup} deletePopup={this.props.deletePopup} deleteBook={this.props.deleteBook} bookList={this.props.bookList} haveBooks={this.props.haveBooks} handleLoan={this.props.handleLoan}/>
         </Fragment>
->>>>>>> 70ba1b72c7d1f4b38c693e4c5885d320487b9944
     );
   }
 }
