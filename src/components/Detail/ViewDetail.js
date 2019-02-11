53c1996c67197d335f9c81941ab850247177ff40
mariaoca <mariaolidcamarena@gmail.com>
2019-02-11 11:48:11 +0100

    styles:add first styles

--- src/components/Detail/ViewDetail.js
+++ src/components/Detail/ViewDetail.js
@@ -3,10 +3,8 @@ import PropTypes from "prop-types";
 import {Link} from 'react-router-dom';
 
 
-
 class ViewDetail extends Component {
   render() {
-    //const {author, title, tags, status, ISBN, type, id} = this.props.item;
     const {bookList} = this.props;
     const bookId = this.props.match.params.id; 
     
@@ -18,23 +16,23 @@ class ViewDetail extends Component {
      return (
        <Fragment>
          <ul className="list__view-details">
-           <li className="list__item--title">
+           <li className="list__item">
              <h2 className="list__item--title">Título</h2>
              <div className="list__item--content" data-detailtitleid={id}>{title}</div>
            </li>
-           <li className="list__item--title">
+           <li className="list__item">
              <h2 className="list__item--title">Autor</h2>
              <div className="list__item--content">{author}</div>
            </li>
-           <li className="list__item--title">
+           <li className="list__item">
              <h2 className="list__item--title">ISBN</h2>
              <div className="list__item--content">{ISBN}</div>
            </li>
-           <li>
+           <li className="list__item">
              <h2 className="list__item--title">Tipo</h2>
              <div className="list__item--content">{type}</div>
            </li>
-           <li className="list__item--title">
+           <li className="list__item">
              <h2 className="list__item--title">Tags</h2>
              <div className="list__item--content">
                <ul className="list__tags--list">
@@ -46,7 +44,7 @@ class ViewDetail extends Component {
                </ul>
              </div>
            </li>
-           <li className="list__item--title">
+           <li className="list__item">
              <h2 className="list__item--title">Estado</h2>
              <div className="list__item--content">{status}</div>
            </li>
