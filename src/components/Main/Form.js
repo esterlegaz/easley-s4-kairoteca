0d4625fbca66e8933d484f0dcde6ff60ac57d257
Ester Legaz <esterlegaz@gmail.com>
2019-02-15 13:22:10 +0100

    fix: solve console bugs

--- src/components/Main/Form.js
+++ src/components/Main/Form.js
@@ -59,26 +59,26 @@ class Form extends Component {
           <form className="form__container" action="/signup" method="post">
             <FormControl className="form__textfield" variant="outlined" required>
               <InputLabel htmlFor="outlined-title">Título</InputLabel>
-              <OutlinedInput id="outlined-title" className="form__input" label="Título" onKeyUp={handleChange} inputProps={{ "data-field": "title" }} />
+              <OutlinedInput labelWidth={0} id="outlined-title" className="form__input" label="Título" onKeyUp={handleChange} inputProps={{ "data-field": "title" }} />
             </FormControl>
 
             <FormControl className="form__textfield" variant="outlined" required>
               <InputLabel htmlFor="outlined-author">Autor</InputLabel>
-              <OutlinedInput className="form__input" label="Autor" id="outlined-author" onKeyUp={handleChange} inputProps={{ "data-field": "author" }} />
+              <OutlinedInput labelWidth={0} className="form__input" label="Autor" id="outlined-author" onKeyUp={handleChange} inputProps={{ "data-field": "author" }} />
             </FormControl>
 
             <FormControl className="form__textfield" variant="outlined" required>
               <InputLabel htmlFor="outlined-ISBN">ISBN</InputLabel>
-              <OutlinedInput className="form__input" label="ISBN" id="outlined-ISBN" onKeyUp={handleChange} inputProps={{ "data-field": "ISBN" }} />
+              <OutlinedInput labelWidth={0} className="form__input" label="ISBN" id="outlined-ISBN" onKeyUp={handleChange} inputProps={{ "data-field": "ISBN" }} />
             </FormControl>
 
             <FormControl className="form__textfield" variant="outlined" required>
               <InputLabel htmlFor="type">Tipo</InputLabel>
-              <Select className="form__input" native value={newBook.type} onChange={handleChange} input={
+              <Select labelWidth={0} className="form__input" native value={newBook.type} onChange={handleChange} input={
                 <OutlinedInput className="form__input" name="type" id="type" inputProps={{ "data-field": "type" }} />}>
-                {types.map(option => {
+                {types.map((option, index) => {
                   return (
-                    <option value={option.value}>{option.label}</option>
+                    <option key={index} value={option.value}>{option.label}</option>
                   )
                 }
                 )}
@@ -93,11 +93,11 @@ class Form extends Component {
 
             <FormControl className="form__textfield" variant="outlined" required>
               <InputLabel htmlFor="status">Estado</InputLabel>
-              <Select className="form__input" native value={newBook.status} onChange={handleChange} input={
+              <Select labelWidth={0} className="form__input" native value={newBook.status} onChange={handleChange} input={
                 <OutlinedInput name="status" id="status" inputProps={{ "data-field": "status" }} />}>
-                {state.map(option => {
+                {state.map((option, optionindex) => {
                   return (
-                    <option value={option.value}>{option.label}</option>
+                    <option key={optionindex} value={option.value}>{option.label}</option>
                   )
                 }
                 )}
