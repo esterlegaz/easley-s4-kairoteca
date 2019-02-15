28affaef56a2c78a535bcd7bfe345d676c1cf51e
Katherine Briceño <kath.gbl@gmail.com>
2019-02-15 11:37:06 +0100

    fix: add prev state

--- src/App.js
+++ src/App.js
@@ -130,10 +130,11 @@ class App extends Component {
 
   handleChange(e){
     const field = e.currentTarget.getAttribute('data-field');
-    const { newBook } = this.state;
-    const addBook = { ...newBook, [field]: e.currentTarget.value }
-    this.setState({
-        newBook: addBook
+    const currentValue = e.currentTarget.value;
+    this.setState((prevState) => {
+      const { newBook } = prevState;
+      const addBook = { ...newBook, [field]: currentValue }
+      return {newBook: addBook}
     });
   }
 
