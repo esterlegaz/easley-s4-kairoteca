ac4864bfeec5d4b0e7033e22b0e079fd06c45435
Katherine Briceño <kath.gbl@gmail.com>
2019-02-18 11:50:51 +0100

    docs: create docs

---
+++ docs/precache-manifest.9ead6eebb5441779b9ccc27412953075.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5110f9536204c325ef05c55f8561f5fe",
+    "url": "./static/media/pencil-black.5110f953.svg"
+  },
+  {
+    "revision": "fadbde648bd8d60762032610c6e90076",
+    "url": "./static/media/close-black-button.fadbde64.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a9f2cbd9689776abb3e3",
+    "url": "./static/js/main.a9f2cbd9.chunk.js"
+  },
+  {
+    "revision": "29b5d1f14c52764a161e",
+    "url": "./static/js/1.29b5d1f1.chunk.js"
+  },
+  {
+    "revision": "a9f2cbd9689776abb3e3",
+    "url": "./static/css/main.388b92e9.chunk.css"
+  },
+  {
+    "revision": "c2b8086e4820e0d1e1548c71d969fa52",
+    "url": "./index.html"
+  }
+];
